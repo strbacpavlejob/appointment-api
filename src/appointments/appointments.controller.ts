65e031a9b1b18a7eacdbe5bac03fac3b52b61b86
strbacpavlejob <strbacpavlejob@gmail.com>
2024-10-27 20:25:29 +0100

    Get appoitments by selected date

--- src/appointments/appointments.controller.ts
+++ src/appointments/appointments.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { AppointmentsService } from './appointments.service';
 
 @Controller('appointments')
-export class AppointmentsController {}
+export class AppointmentsController {
+  constructor(private readonly appointmentsService: AppointmentsService) {}
+
+  @Get('by-date')
+  listByDate(@Query('selectedDate') selectedDate: string) {
+    return this.appointmentsService.listByDate(selectedDate);
+  }
+}
--- src/appointments/appointments.service.ts
+++ src/appointments/appointments.service.ts
@@ -1,4 +1,57 @@
 import { Injectable } from '@nestjs/common';
+import { Appointment } from './appointment.model';
+import * as dayjs from 'dayjs';
 
 @Injectable()
-export class AppointmentsService {}
+export class AppointmentsService {
+  private appointments: Appointment[] = [
+    {
+      id: '1234',
+      startDate: new Date('2024-10-28T09:00:00'),
+      duration: 60,
+      patientName: 'Alice',
+      description: 'check-up',
+    },
+    {
+      id: '1235',
+      startDate: new Date('2024-10-28T10:30:00'),
+      duration: 30,
+      patientName: 'Bob',
+      description: 'vaccination',
+    },
+    {
+      id: '1236',
+      startDate: new Date('2024-10-28T11:00:00'),
+      duration: 45,
+      patientName: 'Sue',
+      description: 'Blood test',
+    },
+    {
+      id: '1237',
+      startDate: new Date('2024-10-28T13:00:00'),
+      duration: 60,
+      patientName: 'Emily',
+      description: 'Consultation',
+    },
+    {
+      id: '1238',
+      startDate: new Date('2024-10-28T14:30:00'),
+      duration: 30,
+      patientName: 'Robert',
+      description: 'Consultation',
+    },
+    {
+      id: '1239',
+      startDate: new Date('2024-10-28T15:00:00'),
+      duration: 60,
+      patientName: 'Linda',
+      description: 'Test',
+    },
+  ];
+
+  listByDate(selectedDate: string): Appointment[] {
+    return this.appointments.filter((app) =>
+      dayjs(app.startDate).isSame(dayjs(selectedDate), 'day'),
+    );
+  }
+}
